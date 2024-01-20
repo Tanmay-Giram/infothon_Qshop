eb73c30d1ea12b5893845178f1f6c31245024bf3
Tanmay <tanmaygiram123@gmail.com>
2024-01-20 10:52:43 +0530

    folder structure & navbar

---
+++ src/app/(user)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../../styles/globals.css";
+import Navbar from "@/components/Navbar";
+
+//import "slick-carousel/slick/slick.css";
+
+export const metadata: Metadata = {
+  title: "Qshop / Quick Shop",
+  description: "online shopping store",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) { 
+  return (
+    <html lang="en">
+      <body className="font-display">
+        <Navbar/>
+    
+        {children}
+      </body>
+    </html>
+  );
+}
