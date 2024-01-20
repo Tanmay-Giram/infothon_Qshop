9fdf8a8a55bc8875d739669c56ea9be991e9aa42
Tanmay <tanmaygiram123@gmail.com>
2024-01-20 18:51:30 +0530

    pagebutton bug persists else working fine

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,10 +7,12 @@ import { IoCloseOutline } from "react-icons/io5";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
 
 const Navbar = () => {
   const pathname = usePathname();
   const [searchQuery, setSearchQuery] = useState("");
+  const { data: session } = useSession();
 
   const navBarList = [
     {
@@ -34,9 +36,9 @@ const Navbar = () => {
       link: "/studio",
     },
     {
-      title: "Map",
+      title: "map",
       link: "/maps",
-    },
+    }
   ];
   return (
     <div className="w-full h-20 bg-white border-b-[1px] border-b-gray-400 sticky top-0 z-50">
@@ -73,6 +75,14 @@ const Navbar = () => {
               {item?.title}
             </Link>
           ))}
+          {session?.user && (
+            <button
+              onClick={() => signOut()}
+              className="flex hover:font-medium w-20 h-6 justify-center items-center px-12 text-gray-500 hover:underline underline-offset-4 decoration-[1px] hover:text-red-600 md:border-r-[2px] border-r-gray-300 duration-200 last:border-r-0"
+            >
+              Logout
+            </button>
+          )}
         </div>
         <HiMenuAlt2 className="inline-flex md:hidden cursor-pointer w-8 h-6" />
       </nav>
