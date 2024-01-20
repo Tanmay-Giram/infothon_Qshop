c632a61f5e42a00b1fc1abc5758cf815ce1969ac
NGW334 <ngw334@gmail.com>
2024-01-21 02:15:55 +0530

    User Profile

---
+++ src/app/(user)/profile/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useState, useEffect } from "react";
+
+const getData = (data: any) => {
+    for (let entry of data) {
+      
+  }
+};
+export default function Profile() {
+  return (
+    <div>
+      <h1>Contact form</h1>
+      <form
+        method="POST"
+        action={getData}
+        className="relative w-fit flex-col h-fit text-base text-primeColor border-[1px] border-black items-center gap-2 justify-evenly px-6 rounded-md"
+      >
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            className="relative flex w-fit h-fit text-base text-primeColor border-[1px] border-black items-center gap-2 justify-between px-6 rounded-md"
+          />
+        </div>
+
+        <div>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            className="relative flex w-fit h-fit text-base text-primeColor border-[1px] border-black items-center gap-2 justify-between px-6 rounded-md"
+          />
+        </div>
+        <div>
+          <label>Shipping Address</label>
+          <textarea
+            name="Shipping Address"
+            className="relative flex w-fit h-fit text-base text-primeColor border-[1px] border-black items-center gap-2 justify-between px-6 rounded-md"
+          ></textarea>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-black align-middle text-slate-100 w-fit h-fit  text-base font-semibold hover:bg-primeColor duration-300"
+        >
+          Send message
+        </button>
+      </form>
+    </div>
+  );
+}
